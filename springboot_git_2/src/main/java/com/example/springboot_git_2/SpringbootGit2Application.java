1ce953bc6dc83df758ab6f1f7d9c179377705acb
Pushpinder <pushpinder.singh99991@gmail.com>
2026-08-01 11:40:57 +0530

    spring boot 2

---
+++ springboot_git_2/src/main/java/com/example/springboot_git_2/SpringbootGit2Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_git_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGit2Application.class, args);
+	}
+
+}
---
+++ springboot_git_2/src/test/java/com/example/springboot_git_2/SpringbootGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_git_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGit2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
